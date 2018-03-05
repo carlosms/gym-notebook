5eb11a9bf933ebc6422496c17a901f488689a162
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-03-05 11:38:56 +0100

    Minor style for exercise table

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -8,45 +8,32 @@ import Button from 'react-toolbox/lib/button/Button'
 
 import { withRouter } from 'react-router-dom';
 
+import "./ExerciseCard.css";
+
 class ExerciseCard extends React.Component {
   handleEditExercise = () => {
     this.props.history.push(`/log/${this.props.date}/${this.props.index}`);
   }
 
   render() {
-    return (
-      <Card>
-        <CardTitle title={this.props.exercise.name}>
-        </CardTitle>
+    return <Card>
+        <CardTitle title={this.props.exercise.name} />
         <CardText>
-          <table>
+          <table class="exercise_card">
             <tbody>
-              {this.props.exercise.sets.map((set, index) =>
-                <tr key={index}>
-                  <td>
-                    {set.weight}
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                    kgs
-                  </td>
-                  <td>
-                    {set.reps}
-                  </td>
-                  <td>
-                    reps
-                  </td>
-                </tr>
-              )}
+              {this.props.exercise.sets.map((set, index) => <tr key={index}>
+                  <td class="exercise_number">{set.weight}</td>
+                  <td class="exercise_label">kgs</td>
+                  <td class="exercise_number">{set.reps}</td>
+                  <td class="exercise_label">reps</td>
+                </tr>)}
             </tbody>
           </table>
         </CardText>
         <CardActions>
           <Button icon="edit" label="Edit" onMouseUp={this.handleEditExercise} />
         </CardActions>
-      </Card>
-    );
+      </Card>;
   }
 }
 
