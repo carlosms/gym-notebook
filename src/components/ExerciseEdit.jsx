20c0964c1bb831d870e9b92d6e631d26f2d81d72
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-03-19 17:20:17 +0100

    Add clear button in ExerciseEdit

--- src/components/ExerciseEdit.jsx
+++ src/components/ExerciseEdit.jsx
@@ -23,7 +23,7 @@ class ExerciseEdit extends React.Component {
 
   exercise = () => {
     return this.props.getExercise(this.props.date, this.props.index);
-  }
+  };
 
   handleChange = (name, value) => {
     if (value < 0) {
@@ -37,49 +37,92 @@ class ExerciseEdit extends React.Component {
     let updated = Object.assign({}, this.exercise());
     updated.sets.push({ weight: this.state.weight, reps: this.state.reps });
     this.props.saveExercise(this.props.date, this.props.index, updated);
-  }
+  };
+
+  handleClear = () => {
+    this.setState({ ...this.state, weight: '', reps: '' });
+  };
 
   render() {
-    const saveStyle = {};
-    const updateStyle = { display: "none" };
-    const deleteStyle = { display: "none" };
+    const saveStyle = { flexGrow: 1 };
+    const clearStyle = { flexGrow: 1 };
+    const updateStyle = { flexGrow: 1,  display: "none" };
+    const deleteStyle = { flexGrow: 1,  display: "none" };
 
-    const disabled = (this.state.weight === '' || this.state.reps === '');
+    const inputStyle = { flexGrow: "1", margin: "0 20px" };
+
+    const disabled = this.state.weight === "" || this.state.reps === "";
 
     // TODO: Select sets from the table, fill values, and show the Update and Delete buttons
 
     return (
-      <React.Fragment >
+      <React.Fragment>
         <Card>
-          <CardTitle title={this.exercise().name}>
-          </CardTitle>
-          <CardText>
-            <Input type='number' label='Weight (kgs)' value={this.state.weight} onChange={this.handleChange.bind(this, 'weight')} />
-            <Input type='number' label='Reps' value={this.state.reps} onChange={this.handleChange.bind(this, 'reps')} />
+          <CardTitle title={this.exercise().name} />
+          <CardText style={{ display: "flex", justifyContent: "center" }}>
+            <div style={inputStyle}>
+              <Input
+                type="number"
+                label="Weight (kgs)"
+                value={this.state.weight}
+                onChange={this.handleChange.bind(this, "weight")}
+              />
+            </div>
+            <div style={inputStyle}>
+              <Input
+                type="number"
+                label="Reps"
+                value={this.state.reps}
+                onChange={this.handleChange.bind(this, "reps")}
+              />
+            </div>
           </CardText>
-          <CardActions>
-            <Button icon="check" label="Save" style={saveStyle} onMouseUp={this.handleSave} primary disabled={disabled} />
-            <Button icon="check" label="Update" style={updateStyle} onMouseUp={this.handleUpdate} primary disabled={disabled} />
-            <Button label="Delete" style={deleteStyle} onMouseUp={this.handleDelete} />
+          <CardActions style={{ width: "80%", margin: "auto", display: "flex", justifyContent: "center" }}>
+            <Button
+              icon="check"
+              label="Save"
+              style={saveStyle}
+              onMouseUp={this.handleSave}
+              primary
+              disabled={disabled}
+            />
+            <Button
+              label="Clear"
+              style={clearStyle}
+              onMouseUp={this.handleClear}
+            />
+            <Button
+              icon="check"
+              label="Update"
+              style={updateStyle}
+              onMouseUp={this.handleUpdate}
+              primary
+              disabled={disabled}
+            />
+            <Button
+              label="Delete"
+              style={deleteStyle}
+              onMouseUp={this.handleDelete}
+            />
           </CardActions>
           <CardText>
             <table className="exercise_card">
               <tbody>
-                {this.exercise().sets.map((set, index) =>
+                {this.exercise().sets.map((set, index) => (
                   <tr key={index}>
                     <td className="exercise_number">{set.weight}</td>
                     <td className="exercise_label">kgs</td>
                     <td className="exercise_number">{set.reps}</td>
                     <td className="exercise_label">reps</td>
                   </tr>
-                )}
+                ))}
               </tbody>
             </table>
           </CardText>
         </Card>
         <br />
         <BackButton />
-      </React.Fragment >
+      </React.Fragment>
     );
   }
 }
